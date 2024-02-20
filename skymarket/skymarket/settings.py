3ad8e1b089190b72513a87ebb237bdd53a1fd4bd
Maxim Abavi <afghan1363@ya.ru>
2024-02-21 02:20:44 +0500

    1. Подключение базы данных Postgres. 2. Подключение CORS headers. 3. Подключение Swagger.

--- skymarket/skymarket/settings.py
+++ skymarket/skymarket/settings.py
@@ -14,7 +14,6 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / '.env')
@@ -154,8 +153,6 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     os.getenv('BACKEND_URL'),
 ]
-
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
