e15c5a8cbc47bc13a1e89dcb2541faa4644b7477
Maxim Abavi <afghan1363@ya.ru>
2024-02-24 14:56:25 +0500

    вернул AUTH_USER_MODEL

--- skymarket/skymarket/settings.py
+++ skymarket/skymarket/settings.py
@@ -80,6 +80,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "skymarket.wsgi.application"
 
+AUTH_USER_MODEL = 'users.User'
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
